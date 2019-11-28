4f18f2a8d5e03dddf21ff683f6dd873a8b4cf18f
Jovanni Lo <lodev09@gmail.com>
2019-11-29 00:15:52 +0800

    added sample db

---
+++ src/lodev09.sql
@@ -0,0 +1,99 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for osx10.14 (x86_64)
+--
+-- Host: localhost    Database: lodev09
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `links`
+--
+
+DROP TABLE IF EXISTS `links`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `links` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
+  `profile_id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(128) DEFAULT NULL,
+  `url` varchar(128) DEFAULT NULL,
+  `icon` varchar(128) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_active` (`active`) USING BTREE,
+  KEY `idx_created_at` (`created_at`) USING BTREE,
+  KEY `idx_updated_at` (`updated_at`) USING BTREE,
+  KEY `idx_name` (`name`) USING BTREE,
+  KEY `profile_id` (`profile_id`),
+  CONSTRAINT `links_ibfk_1` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `links`
+--
+
+LOCK TABLES `links` WRITE;
+/*!40000 ALTER TABLE `links` DISABLE KEYS */;
+INSERT INTO `links` VALUES (1,1,1,'twitter','https://twitter.com/lodev09','fab fa-twitter','2018-12-31 02:25:39','2018-12-31 02:25:55'),(2,1,1,'linkedin','https://www.linkedin.com/in/lodev09/','fab fa-linkedin-in','2018-12-31 02:26:17','2018-12-31 02:26:17'),(3,1,1,'github','https://github.com/lodev09','fab fa-github','2018-12-31 02:26:46','2018-12-31 02:27:12'),(4,1,1,'facebook','https://www.facebook.com/lodev09','fab fa-facebook-f','2018-12-31 02:27:02','2018-12-31 02:27:21');
+/*!40000 ALTER TABLE `links` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
+  `firstname` varchar(128) DEFAULT NULL,
+  `lastname` varchar(128) DEFAULT NULL,
+  `address` varchar(128) DEFAULT NULL,
+  `mobile` varchar(128) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `about` text,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_active` (`active`) USING BTREE,
+  KEY `idx_created_at` (`created_at`) USING BTREE,
+  KEY `idx_email` (`email`) USING BTREE,
+  KEY `idx_updated_at` (`updated_at`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profile`
+--
+
+LOCK TABLES `profile` WRITE;
+/*!40000 ALTER TABLE `profile` DISABLE KEYS */;
+INSERT INTO `profile` VALUES (1,1,'Joemary','Lo','Dumaguete City, Philippines','111111','lodev09@gmail.com','Highly-skilled full stack developer bringing more than 6 years in software design, development and integration. Advanced knowledge of PHP, MySQL, Javascript, and React/Redux. Passionate about quality, Fast learner and loves challenges.','2019-11-28 23:09:10','2018-12-31 02:16:01');
+/*!40000 ALTER TABLE `profile` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-29  0:15:40
